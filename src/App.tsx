f09b525474eab6c0ce64bc3bed1d990491b7eac3
Matias <matiasdn@metropolia.fi>
2023-04-02 12:15:55 +0300

    Create initial structure for real-time chat feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
 function App() {
   return (
-      <div>
-        <h1>Hello World!</h1>
-      </div>
+    <div>
+      <h1>Real-time Chat App</h1>
+      <Chat />
+    </div>
   );
 }
+
+const Chat: React.FC = () => {
+  const [messages, setMessages] = useState<string[]>([]);
+  const [input, setInput] = useState("");
+
+  useEffect(() => {
+    // Connect to the server
+    // add received messages to the state
+    setMessages(["Hello", "World"]);
+  }, []);
+
+  const sendMessage = () => {
+    if (input.trim()) {
+      // Send message to the server
+      setMessages([...messages, input]);
+      setInput("");
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        {messages.map((message, index) => (
+          <Message key={index} content={message} />
+        ))}
+      </div>
+      <div>
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button onClick={sendMessage}>Send</button>
+      </div>
+    </div>
+  );
+};
+
+interface MessageProps {
+  content: string;
+}
+
+const Message: React.FC<MessageProps> = ({ content }) => {
+  return <div>{content}</div>;
+};
 
 export default App;
