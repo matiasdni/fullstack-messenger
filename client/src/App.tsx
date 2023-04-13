57067a5cee0ff4d8a7bad05553ea90f05132169d
Matias <matiasdn@metropolia.fi>
2023-04-13 05:47:50 +0300

    implement routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,77 +1,22 @@
 import React from "react";
-import Login from "./components/Login";
-import { Route, Routes } from "react-router-dom";
-import { Home } from "./components/Home";
+import { Route, Routes } from "react-router";
 import PrivateRoute from "./components/PrivateRoute";
+import { Home } from "./components/Home";
+import { LoginScreen } from "./LoginScreen";
+import DarkModeToggle from "./components/DarkModeToggle";
 
 function App(): JSX.Element {
-  const [user, setUser] = React.useState(null);
-
-  // const handleLogin = () => setUser({ id: "1", name: "robin" });
-  // const handleLogout = () => setUser(null);
-
   return (
-    <Routes>
-      <Route path="/" element={<PrivateRoute />}>
-        <Route path="/" element={<Home />} />
-      </Route>
-      <Route path="/login" element={<Login />} />
-    </Routes>
+    <div className="dark:bg-gray-900 bg-gray-100">
+      <DarkModeToggle />
+      <Routes>
+        <Route path="/" element={<PrivateRoute />}>
+          <Route element={<Home />} />
+        </Route>
+        <Route path="/login" element={<LoginScreen />} />
+      </Routes>
+    </div>
   );
 }
 
 export default App;
-
-//   return (
-//     <div className="App">
-//       <DarkModeToggle />
-//       <div className="bg-cover dark:bg-gray-900">
-//         {user ? (
-//           <button onClick={handleLogout}>Sign Out</button>
-//         ) : (
-//           <button onClick={handleLogin}>Sign In</button>
-//         )}
-//         <Routes>
-//           <Route
-//             path="home"
-//             element={
-//               <PrivateRoute user={user}>
-//                 <Home />
-//               </PrivateRoute>
-//             }
-//           />
-//           {/*<PrivateRoute path="/dashboard" element={<Dashboard />} />*/}
-//           {!user && <Route path="/login" Component={Login} />}
-//           {user && <Route path="/dashboard" Component={Dashboard} />}
-//         </Routes>
-//       </div>
-//     </div>
-//   );
-// }
-
-/*
-// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-// import { AuthProvider } from "./contexts/AuthContext";
-// import Signup from "./components/Signup";
-// import ForgotPassword from "./components/ForgotPassword";
-// import UpdateProfile from "./components/UpdateProfile";
-// import Dashboard from "./components/Dashboard";
-// import Home from "./components/Home";
-// import About from "./components/About";
-// import Contact from "./components/Contact";
-// import NotFound from "./components/NotFound";
-// <Router>
-//   <AuthProvider>
-//     <Switch>
-//       <PrivateRoute exact path="/" component={Home} />
-//       <PrivateRoute path="/dashboard" component={Dashboard} />
-//       <PrivateRoute path="/update-profile" component={UpdateProfile} />
-//       <Route path="/login" component={Login} />
-//       <Route path="/signup" component={Signup} />
-//       <Route path="/forgot-password" component={ForgotPassword} />
-//       <Route path="/about" component={About} />
-//       <Route path="/contact" component={Contact} />
-//       <Route path="*" component={NotFound} />
-//     </Switch>
-//   </AuthProvider>
-// </Router>*/
