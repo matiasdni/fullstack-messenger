36724b4feca523503836885d7f99cad649dd4f66
Matias <matiasdn@metropolia.fi>
2023-04-16 04:26:49 +0300

    refactor

--- server/src/config.ts
+++ server/src/config.ts
@@ -2,8 +2,8 @@ import * as dotenv from "dotenv";
 
 dotenv.config();
 
-export const config: any = {
-  development: {
+export const config = {
+  database: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
@@ -11,13 +11,9 @@ export const config: any = {
     port: Number(process.env.DB_PORT),
     dialect: "postgres",
   },
-  test: {
-    // Test database configuration
-  },
-  production: {
-    // Production database configuration
-  },
   jwt: {
     secret: process.env.JWT_SECRET || "secret",
   },
 };
+
+export const jwtSecret = config.jwt.secret;
