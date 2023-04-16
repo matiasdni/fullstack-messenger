f8a040f25122bb71f3c5e485bee736d7496b094d
Matias <matiasdn@metropolia.fi>
2023-04-16 04:26:34 +0300

    implement new paths

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,15 +3,20 @@ import cors from "cors";
 
 const loginRouter = require("./controllers/login");
 const usersRouter = require("./controllers/users");
+const chatRouter = require("./controllers/chat");
 
 const app: Express = express();
 
+app.use(express.json());
+
 app.use(cors());
 
-app.use(express.json());
+app.use("/api/register", usersRouter);
 
 app.use("/api/login", loginRouter);
 
 app.use("/api/users", usersRouter);
+
+app.use("/api/chat", chatRouter);
 
 export default app;
