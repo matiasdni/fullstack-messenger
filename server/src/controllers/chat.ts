8ccb1bb2fe30687841e73b4336ce79ace4903c32
Matias <matiasdn@metropolia.fi>
2023-04-18 05:17:30 +0300

    use scopes to return all chat data

--- server/src/controllers/chat.ts
+++ server/src/controllers/chat.ts
@@ -1,36 +1,15 @@
 import { Response } from "express";
 import authenticate, { AuthRequest } from "../middlewares/auth";
-import { Message } from "../models/message";
-import { User } from "../models/user";
 
 const router = require("express").Router();
 
 router.get("/", authenticate, async (req: AuthRequest, res: Response) => {
   const user = req.user;
   if (user) {
-    // get user's chats
     const chats = await user.getChats({
-      nest: true,
-      attributes: ["id", "name", "description", "updatedAt"],
-      include: [
-        {
-          model: User,
-          attributes: ["id", "username"],
-          through: { attributes: [] },
-        },
-        {
-          model: Message,
-          attributes: ["id", "content", "createdAt"],
-          order: [["createdAt", "DESC"]],
-          include: [
-            {
-              model: User,
-              attributes: ["username"],
-            },
-          ],
-        },
-      ],
+      scope: "withUsersAndMessages",
     });
+    console.log(chats);
     res.send(chats).status(200);
   }
 });
