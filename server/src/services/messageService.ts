3f2889866c8ba466414d3694c94e2ce3bc5d4e21
matiasdn <matiasdn@metropolia.fi>
2023-04-19 21:59:56 +0300

    remove unnecessary parameters

--- server/src/services/messageService.ts
+++ server/src/services/messageService.ts
@@ -2,8 +2,8 @@ import { User } from "../models/user";
 import { Chat } from "../models/chat";
 import { Message } from "../models/message";
 
-export async function createMessage(content: string, user: User, chat: Chat) {
-  return await Message.create({ content, chat_id: chat.id, user_id: user.id });
+export async function createMessage(content, userId, chatId) {
+  return await Message.create({ content, chat_id: chatId, user_id: userId });
 }
 
 export async function getMessages(chat: Chat) {
