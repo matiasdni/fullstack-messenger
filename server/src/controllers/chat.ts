0536b9fcf47ab1263c1f862d616c83d4eb5cda6e
Matias <matiasdn@metropolia.fi>
2023-04-20 04:40:15 +0300

    fixed

--- server/src/controllers/chat.ts
+++ server/src/controllers/chat.ts
@@ -1,14 +1,17 @@
 import { Response } from "express";
 import authenticate, { AuthRequest } from "../middlewares/auth";
+import { User } from "../models/user";
 
 const router = require("express").Router();
 
 router.get("/", authenticate, async (req: AuthRequest, res: Response) => {
   const user = req.user;
   if (user) {
-    const chats = await user.getChats({
-      scope: "withUsersAndMessages",
-    });
+    const user1: any = await User.scope("chatsWithOrderedMessages").findByPk(
+      user.dataValues.id
+    );
+    const chats = user1.get({ plain: true }).chats;
+    console.log(chats);
     res.send(chats).status(200);
   }
 });
