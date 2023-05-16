90f45132caf3510d5aa9c4b31a948ee2872e2b61
Matias <matiasdn@metropolia.fi>
2023-05-16 21:28:10 +0300

    refactor code

--- client/src/components/MessageList.tsx
+++ client/src/components/MessageList.tsx
@@ -1,19 +1,17 @@
 import { Message } from "./Message";
 import React, { useEffect, useRef } from "react";
-import { useAppSelector } from "../store";
-import { selectActiveChat } from "../features/chats/chatsSlice";
+import { ChatHeaderProps } from "./ChatHeader";
 
-export const MessageList: React.FC = () => {
+export const MessageList: React.FC<ChatHeaderProps> = ({ activeChat }) => {
   const messagesEndRef = useRef<HTMLDivElement | null>(null);
-  const activeChat = useAppSelector(selectActiveChat);
 
   useEffect(() => {
     messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
-  }, [activeChat.messages]);
+  }, [activeChat?.messages]);
 
   return (
     <div className="flex-1 overflow-y-auto px-3 py-2">
-      {activeChat.messages?.map((message, index) => (
+      {activeChat?.messages?.map((message, index) => (
         <Message key={index} message={message} />
       ))}
       <div ref={messagesEndRef} />
