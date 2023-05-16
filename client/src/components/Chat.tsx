3de9929eb025e3cbd307401d7a0217f703b31725
Matias <matiasdn@metropolia.fi>
2023-05-16 21:25:03 +0300

    remove unnecessary code

--- client/src/components/Chat.tsx
+++ client/src/components/Chat.tsx
@@ -1,23 +1,13 @@
-import { useEffect, useState } from "react";
 import { socket } from "../socket";
 import { useAppSelector } from "../store";
 import { selectActiveChat } from "../features/chats/chatsSlice";
-import { Chat as ChatType, Message } from "../features/chats/types";
+import { Chat as ChatType } from "../features/chats/types";
 import { ChatHeader } from "./ChatHeader";
 import { MessageList } from "./MessageList";
 import { ChatInputForm } from "./ChatInputForm";
 
 export const Chat = () => {
   const activeChat: ChatType = useAppSelector(selectActiveChat);
-  const [messages, setMessages] = useState<Message[]>([]);
-
-  useEffect(() => {
-    setMessages(activeChat?.messages);
-
-    return () => {
-      socket.off("message");
-    };
-  }, [activeChat.messages]);
 
   const sendMessage = (input) => {
     console.log("sending message");
@@ -33,15 +23,21 @@ export const Chat = () => {
     }
   };
 
-  return activeChat?.name ? (
+  return activeChat ? (
     <section className="absolute inset-0 flex h-full flex-col overflow-hidden border">
       <ChatHeader activeChat={activeChat} />
 
       {/*messages*/}
-      <MessageList />
+      <MessageList activeChat={activeChat} />
 
       {/*input*/}
       <ChatInputForm onSubmit={sendMessage} />
     </section>
-  ) : null;
+  ) : (
+    <section className="absolute inset-0 flex h-full flex-col overflow-hidden border">
+      <ChatHeader />
+      {/*messages*/}
+      <MessageList />
+    </section>
+  );
 };
