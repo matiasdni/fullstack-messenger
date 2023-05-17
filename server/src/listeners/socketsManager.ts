bfac7fccff723e2929b0678edc8a15ff173d0d93
Matias <matiasdn@metropolia.fi>
2023-05-17 08:05:36 +0300

    implement new listeners/logic to private chats

--- server/src/listeners/socketsManager.ts
+++ server/src/listeners/socketsManager.ts
@@ -1,5 +1,10 @@
 import { io } from "../server";
 import { mySocket } from "./types";
+import { getUserById } from "../services/userService";
+import { createChat } from "../services/chatService";
+import { Chat } from "../models/chat";
+import { User } from "../models/user";
+import { Message } from "../models/message";
 
 const onConnection = (socket: mySocket) => {
   const { joinRoom, sendMessage, leaveRoom, searchUser } =
@@ -21,6 +26,87 @@ const onConnection = (socket: mySocket) => {
   socket.on("message", sendMessage);
   socket.on("leave-room", leaveRoom);
   socket.on("search:user", searchUser);
+  socket.on("get:chatByUserId", async (data) => {
+    const otherUser = await getUserById(data);
+    const currentUser = socket.user;
+    if (!otherUser || !currentUser || otherUser.id === currentUser.id) return;
+
+    // Fetch the private chat between the current user and the other user
+    const existingChat = await Chat.findOne({
+      where: { chat_type: "private" },
+      include: [
+        {
+          model: User,
+          where: { id: [currentUser.id, otherUser.id] },
+          through: { attributes: [] },
+          as: "users",
+        },
+      ],
+    });
+
+    if (existingChat) {
+      socket.emit("get:chatByUserId", existingChat);
+    } else {
+      const newChat = await createChat(
+        `${currentUser.username}-${otherUser.username}`,
+        undefined,
+        "private"
+      );
+      await newChat.addUser(currentUser);
+      await newChat.addUser(otherUser);
+
+      // include users etc in the response
+      const chat = (await Chat.findByPk(newChat.id, {
+        include: [
+          {
+            model: User,
+            through: { attributes: [] },
+            as: "users",
+          },
+          {
+            model: Message,
+            as: "messages",
+            include: [
+              {
+                model: User,
+                attributes: ["id", "username"],
+                as: "user",
+              },
+            ],
+          },
+        ],
+      })) as Chat;
+
+      // socket.to(currentUser.id).emit("join-room", chat.id);
+      io.to(currentUser.id).emit("join-room", chat.id);
+      socket.to(otherUser.id).emit("join-room", chat.id); // Make the other user join the new chat room
+      socket.emit("get:chatByUserId", chat);
+    }
+  });
+
+  socket.on("get:chatById", async (data) => {
+    const chat = await Chat.findByPk(data, {
+      include: [
+        {
+          model: User,
+          through: { attributes: [] },
+          as: "users",
+        },
+        {
+          model: Message,
+          as: "messages",
+          include: [
+            {
+              model: User,
+              attributes: ["id", "username"],
+              as: "user",
+            },
+          ],
+        },
+      ],
+    });
+    socket.emit("get:chatById", chat);
+  });
 
   socket.on("disconnect", () => {
     console.log("User disconnected: ", user.username);
