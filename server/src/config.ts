169208ee939a89fd8f5a8aba3d4a13ca19e0afed
matiasdn <matiasdn@metropolia.fi>
2023-05-18 13:19:18 +0300

    db config to use local db in development

--- server/src/config.ts
+++ server/src/config.ts
@@ -2,18 +2,25 @@ import * as dotenv from "dotenv";
 
 dotenv.config();
 
+const isDevelopment = process.env.NODE_ENV !== "production";
+
 export const config = {
   database: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    dialect: "postgres",
+    username: isDevelopment ? process.env.DB_USER : process.env.PROD_DB_USER,
+    password: isDevelopment
+      ? process.env.DB_PASSWORD
+      : process.env.PROD_DB_PASSWORD,
+    database: isDevelopment ? process.env.DB_NAME : process.env.PROD_DB_NAME,
+    host: isDevelopment ? process.env.DB_HOST : process.env.PROD_DB_HOST,
+    port: Number(
+      isDevelopment ? process.env.DB_PORT : process.env.PROD_DB_PORT
+    ),
+    dialect: isDevelopment
+      ? process.env.DB_DIALECT
+      : process.env.PROD_DB_DIALECT || "postgres",
   },
   jwt: {
     secret: process.env.JWT_SECRET || "secret",
   },
 };
-
 export const jwtSecret = config.jwt.secret;
