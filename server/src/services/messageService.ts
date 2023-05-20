2e59654c435a2d76c84b4118fd9dbecbb4f5c1c1
Matias <matiasdn@metropolia.fi>
2023-05-20 22:11:24 +0300

    method for messageService.ts

--- server/src/services/messageService.ts
+++ server/src/services/messageService.ts
@@ -1,4 +1,3 @@
-import { Chat } from "../models/chat";
 import { Message } from "../models/message";
 
 export async function createMessage(
@@ -7,8 +6,4 @@ export async function createMessage(
   chatId: string
 ) {
   return await Message.create({ content, chat_id: chatId, user_id: userId });
-}
-
-export async function getMessages(chat: Chat) {
-  return await chat.getMessages();
 }
