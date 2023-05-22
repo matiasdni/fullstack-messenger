f73d38fb1d2af9add570efdd0c9a8a13f002308a
Matias <matiasdn@metropolia.fi>
2023-05-23 01:22:48 +0300

    reimplementing user search with react-select

--- client/src/components/GroupForm.tsx
+++ client/src/components/GroupForm.tsx
@@ -1,10 +1,10 @@
 import { useAppDispatch, useAppSelector } from "../store";
-import React, { FC, useEffect, useRef, useState } from "react";
+import React, { useCallback, useState } from "react";
 import { User } from "../features/users/types";
 import { ChatType, createChat } from "../features/chats/chatsSlice";
-import { Combobox } from "@headlessui/react";
 import { searchUsersByName } from "../services/user";
-import { debounce } from "../utils/debounce";
+import AsyncSelect from "react-select/async";
+import OptionTypeBase from "react-select";
 
 export type GroupChat = {
   name: string;
@@ -13,92 +13,89 @@ export type GroupChat = {
   chat_type: ChatType.Group;
 };
 
-const AddUsers: FC = () => {
-  const [selected, setSelected] = useState<User[]>([]);
-  const [search, setSearch] = useState<string>("");
-  const [options, setOptions] = useState<User[]>([]);
-  const [isFocused, setIsFocused] = useState<boolean>(false);
-  const {
-    user: { id: userId },
-    token,
-  } = useAppSelector((state) => state.auth);
-  const cache = useRef<{ [key: string]: User[] }>({});
+interface UserOption extends OptionTypeBase {
+  label: string;
+  value: User;
+  avatar: string;
+}
 
-  const fetchData = () => {
-    if (search) {
-      if (search in cache.current) {
-        console.log("cached");
-        setOptions(cache.current[search]);
-      } else {
-        console.log("searching");
-        searchUsersByName(search, token).then((res: User[]) => {
-          cache.current[search] = res;
-          setOptions(res);
-        });
+const userToOption = (user: User): UserOption =>
+  ({
+    label: user.username,
+    value: user,
+  } as UserOption);
+
+const formatOptionLabel = ({ value, label, avatar }) => (
+  <div className="flex flex-row items-center">
+    <img className="inline-block h-8 w-8 rounded-full" src={avatar} alt="" />
+    <p className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+      {label}
+    </p>
+  </div>
+);
+
+interface UserSelectProps {
+  selectedUsers: UserOption[];
+  onUserSelect: (selected: User[]) => void;
+  searchUsersByName: (
+    inputValue: string,
+    token: string
+  ) => Promise<UserOption[]>;
+  token: string;
+}
+
+const UserSelect = ({ onUserSelect, searchUsersByName, token }) => {
+  const loadOptions = useCallback(
+    async (inputValue): Promise<UserOption[]> => {
+      if (!inputValue) {
+        return [];
       }
-    } else {
-      setOptions([]);
-    }
-  };
-
-  useEffect(() => {
-    const debouncedSearch = debounce(fetchData, 500);
-    debouncedSearch();
-  }, [search]);
-
-  const handleUserSelect = (user) => {
-    console.log(user);
-    setSelected([...selected, user]);
-  };
-
-  useEffect(() => {
-    console.log(selected);
-    setOptions(options.filter((u) => !selected.find((s) => s.id === u.id)));
-  }, [selected]);
-
-  const filteredOptions = options.filter(
-    (user) => !selected.find((u) => u.id === user.id) && user.id !== userId
+      const users = await searchUsersByName(inputValue, token);
+      return users.map(userToOption);
+    },
+    [searchUsersByName, token]
   );
+
+  return (
+    <AsyncSelect
+      isMulti
+      cacheOptions
+      loadOptions={loadOptions}
+      onChange={onUserSelect}
+      closeMenuOnSelect={false}
+      noOptionsMessage={() => null}
+      getOptionLabel={(option) => option.label}
+      getOptionValue={(option) => option.value}
+      formatOptionLabel={formatOptionLabel}
+      styles={{
+        input: (base) => ({
+          ...base,
+          "input:focus": {
+            boxShadow: "none",
+          },
+        }),
+      }}
+    />
+  );
+};
+
+const AddUsers = ({ setSelectedUsers }) => {
+  const [selected, setSelected] = useState([]);
+  const { token } = useAppSelector((state) => state.auth);
+
+  const handleUserSelect = (selected) => {
+    setSelected(selected);
+    setSelectedUsers(selected.map((option) => option.value));
+  };
 
   return (
     <div className="flex flex-col">
       <p className="text-sm">Add users</p>
-      <Combobox
-        multiple={true}
-        // value={selected}
-        onChange={(e) => handleUserSelect(e)}
-      >
-        <Combobox.Input
-          className="w-full rounded-md border border-gray-300 p-2 dark:border-gray-700"
-          placeholder="Search users"
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
-          onBlur={() => setIsFocused(false)}
-          onFocus={() => setIsFocused(true)}
-        />
-        <div className="z-10 mt-2 max-h-60 w-full">
-          <div className="rounded-md bg-white shadow-lg dark:bg-gray-800">
-            <Combobox.Options>
-              {options.map((user) => (
-                <Combobox.Option value={user} key={user.id}>
-                  <div className="cursor-pointer flex-row items-center justify-between p-2 hover:bg-gray-100 dark:hover:bg-gray-700">
-                    <div className="flex flex-row items-center">
-                      <img
-                        className="inline-block h-8 w-8 rounded-full"
-                        src={`https://avatars.dicebear.com/api/identicon/${user.username}.svg`}
-                        alt=""
-                      />
-                      <p className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-                        {user.username}
-                      </p>
-                    </div>
-                  </div>
-                </Combobox.Option>
-              ))}
-            </Combobox.Options>
-          </div>
-        </div>
-      </Combobox>
+      <UserSelect
+        onUserSelect={handleUserSelect}
+        searchUsersByName={searchUsersByName}
+        token={token}
+      />
     </div>
   );
 };
@@ -109,15 +106,8 @@ export const GroupForm = (props: { handleCloseModal: () => void }) => {
   const [selectedUsers, setSelectedUsers] = useState<User[]>([]);
   const [groupDescription, setGroupDescription] = useState<string>("");
 
-  const handleUserSelect = (user: User) => {
-    setSelectedUsers((prev) => [...prev, user]);
-  };
-
-  const handleUserDeselect = (user: User) => {
-    setSelectedUsers((prev) => prev.filter((u) => u.id !== user.id));
-  };
-
   const handleCreateGroup = async () => {
+    console.log(selectedUsers);
     const chat: GroupChat = {
       name: groupName,
       description: groupDescription,
@@ -125,8 +115,11 @@ export const GroupForm = (props: { handleCloseModal: () => void }) => {
       chat_type: ChatType.Group,
     };
 
-    await dispatch(createChat(chat));
-    props.handleCloseModal();
+    const action = await dispatch(await createChat(chat));
+    if (createChat.fulfilled.match(action)) {
+      console.log("Group created");
+      console.log(action.payload);
+    }
   };
 
   return (
@@ -153,27 +146,7 @@ export const GroupForm = (props: { handleCloseModal: () => void }) => {
         onChange={(e) => setGroupDescription(e.target.value)}
       />
       <div className={`relative flex-1`}>
-        <AddUsers />
-      </div>
-
-      <div className="flex flex-col gap-2">
-        <p className="text-sm">Selected users</p>
-        <div className="flex flex-col gap-2">
-          {selectedUsers.map((user) => (
-            <div
-              key={user.id}
-              className="flex flex-row items-center justify-between rounded-md border border-gray-300 p-2 dark:border-gray-700"
-            >
-              <p>{user.username}</p>
-              <button
-                className="text-red-500"
-                onClick={() => handleUserDeselect(user)}
-              >
-                X
-              </button>
-            </div>
-          ))}
-        </div>
+        <AddUsers setSelectedUsers={setSelectedUsers} />
       </div>
       <div className="flex flex-row gap-2">
         <button
