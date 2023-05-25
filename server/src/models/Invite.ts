89e89347e253531b208cc80438a5a76046f6c481
Matias <matiasdn@metropolia.fi>
2023-05-25 07:35:01 +0300

    change fle name

---
+++ server/src/models/Invite.ts
@@ -0,0 +1,60 @@
+import {
+  CreationOptional,
+  DataTypes,
+  ForeignKey,
+  InferAttributes,
+  InferCreationAttributes,
+  Model,
+  Sequelize,
+} from "sequelize";
+import { User } from "./user";
+import { Chat } from "./chat";
+
+class Invite extends Model<
+  InferAttributes<Invite>,
+  InferCreationAttributes<Invite>
+> {
+  declare id: CreationOptional<string>;
+  declare status: CreationOptional<string>;
+  declare sender_id: ForeignKey<User["id"]>;
+  declare chat_id: ForeignKey<Chat["id"]>;
+  declare recipient_id: ForeignKey<User["id"]>;
+
+  declare readonly createdAt: CreationOptional<Date>;
+  declare readonly updatedAt: CreationOptional<Date>;
+}
+
+const initInvitation = (sequelize: Sequelize): typeof Invite =>
+  Invite.init(
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+        allowNull: false,
+      },
+      status: {
+        type: DataTypes.ENUM,
+        values: ["pending", "accepted", "declined"],
+        allowNull: false,
+        defaultValue: "pending",
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+      },
+    },
+    {
+      sequelize,
+      tableName: "invite",
+      underscored: true,
+    }
+  );
+
+export { initInvitation, Invite };
