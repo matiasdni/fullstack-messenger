d52f6576c0740ac44bee7e80cdf69efcd9a7dfae
Matias <matiasdn@metropolia.fi>
2023-05-27 02:55:06 +0300

    types for user and invite

--- client/src/features/users/types.ts
+++ client/src/features/users/types.ts
@@ -1,7 +1,19 @@
-export type User = {
+export type Status = "pending" | "accepted" | "rejected";
+
+export interface Invite {
+  id: string;
+  sender: User;
+  receiver?: User | string;
+  chat_id?: string;
+  status: Status;
+}
+
+export interface User {
+  friends?: User[];
+  invites?: Invite[];
   id: string;
   username: string;
   role: string | null;
-};
+}
 
 export type UsersInitialState = User[];
