2faab33aec18cd7ad4ff4df7983533707d45e122
Matias <matiasdn@metropolia.fi>
2023-05-27 02:54:38 +0300

    fix styling

--- client/src/components/sidebar/SidebarHeader.tsx
+++ client/src/components/sidebar/SidebarHeader.tsx
@@ -25,40 +25,38 @@ export const SidebarHeader: FC<SidebarHeaderProps> = ({
 
   return (
     <>
-      <div className="w-full bg-gray-200 px-3 py-2 dark:bg-gray-800">
-        <div className="flex items-center">
+      <div className="space-y-2 bg-neutral-100 px-3 py-2 dark:bg-gray-800/70">
+        <div className="mx-auto flex items-center space-x-3 text-xl">
           <div className="h-10 w-10">
             <Avatar />
           </div>
-          <div className="w-3"></div>
-          <h1 className="text-center text-xl">
+          <h1>
             Welcome, <span>{user?.username}</span>
           </h1>
+          <div className="flex-1"></div>
+          <span
+            className="cursor-pointer p-2"
+            onClick={() => {
+              setIsGroupModalOpen(true);
+            }}
+          >
+            <svg
+              viewBox="0 0 1024 1024"
+              className="h-6 w-6 fill-current hover:fill-gray-700 dark:fill-gray-400 dark:hover:fill-gray-300"
+            >
+              <path d="M482 152h60q8 0 8 8v704q0 8-8 8h-60q-8 0-8-8V160q0-8 8-8z" />
+              <path d="M176 474h672q8 0 8 8v60q0 8-8 8H176q-8 0-8-8v-60q0-8 8-8z" />
+            </svg>
+          </span>
         </div>
         <button
-          className="max-w-md cursor-pointer rounded border-2 border-gray-300 px-3 py-1 text-left dark:border-gray-700"
+          className="min-w-full cursor-pointer rounded border border-gray-400 bg-neutral-100 px-3 py-1 text-left shadow-sm dark:border-gray-600 dark:bg-gray-700"
           onClick={() => {
             setIsModalOpen(true);
           }}
         >
           Start a new chat...
         </button>
-
-        <div
-          className="cursor-pointer justify-self-end"
-          onClick={() => {
-            setIsGroupModalOpen(true);
-          }}
-        >
-          <svg
-            viewBox="0 0 1024 1024"
-            fill="currentColor"
-            className="h-6 w-6 text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300"
-          >
-            <path d="M482 152h60q8 0 8 8v704q0 8-8 8h-60q-8 0-8-8V160q0-8 8-8z" />
-            <path d="M176 474h672q8 0 8 8v60q0 8-8 8H176q-8 0-8-8v-60q0-8 8-8z" />
-          </svg>
-        </div>
       </div>
 
       {isModalOpen && (
@@ -79,7 +77,6 @@ export const SidebarHeader: FC<SidebarHeaderProps> = ({
           />
         </Modal>
       )}
-      <div className="h-2"></div>
 
       <SidebarTabs activeTab={activeTab} onChangeTab={onChangeTab} />
     </>
