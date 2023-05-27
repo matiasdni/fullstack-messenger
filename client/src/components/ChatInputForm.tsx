5d7310ba56c1f665b2941ab0475ba5aca436fc53
Matias <matiasdn@metropolia.fi>
2023-05-27 06:46:23 +0300

    styling

--- client/src/components/ChatInputForm.tsx
+++ client/src/components/ChatInputForm.tsx
@@ -17,9 +17,23 @@ export const ChatInputForm: React.FC<InputFormProps> = ({ onSubmit }) => {
 
   return (
     <form
-      className="flex items-center justify-between space-x-2 bg-transparent p-3"
+      className="flex items-center justify-between space-x-2 rounded-lg bg-neutral-200/50 p-4 dark:bg-gray-800"
       onSubmit={handleSubmit}
     >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-8 w-8 fill-neutral-600 dark:text-gray-700"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="{2}"
+          d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+        />
+      </svg>
+
       <input
         type="text"
         className="focus:shadow-outline form-input w-full appearance-none rounded-lg border border-neutral-300 bg-neutral-100 px-4 py-2 leading-normal focus:outline-none dark:border-gray-700 dark:bg-gray-700"
