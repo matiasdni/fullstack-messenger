956c3a19522108a8eae6123155b53babca1fdd5f
Matias <matiasdn@metropolia.fi>
2023-05-29 02:15:35 +0300

    add invites to RootState

--- client/src/types.ts
+++ client/src/types.ts
@@ -1,9 +1,11 @@
 import { ChatState } from "./features/chats/types";
 import { User } from "./features/users/types";
 import { AuthState } from "./features/auth/types";
+import { Invite } from "./features/invites/types";
 
 export type RootState = {
   chats: ChatState;
   users: User[];
   auth: AuthState;
+  invites: Invite[];
 };
