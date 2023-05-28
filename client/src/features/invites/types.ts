b576b7293cec3892d9d5b590aec0303ba0f67eef
Matias <matiasdn@metropolia.fi>
2023-05-29 02:16:05 +0300

    move invite interface to same dir with inviteslice

---
+++ client/src/features/invites/types.ts
@@ -0,0 +1,13 @@
+import { Status } from "../../../../shared/types";
+import { User } from "../users/types";
+
+export interface Invite {
+  id: string;
+  sender: User;
+  chat: {
+    id: string;
+    name: string;
+  };
+  status?: Status;
+  createdAt: Date;
+}
