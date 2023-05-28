5d327434304f0f3b818599ad7aeb037d19bcd983
Matias <matiasdn@metropolia.fi>
2023-05-28 04:44:47 +0300

    refactor useChats and useActiveChat hooks

--- client/src/hooks/useChats.ts
+++ client/src/hooks/useChats.ts
@@ -1,13 +1,29 @@
 import { useSelector } from "react-redux";
+import { setActiveChat } from "src/features/chats/chatsSlice";
 import { Chat, ChatState } from "src/features/chats/types";
+import { useAppDispatch } from "src/store";
 import { RootState } from "src/types";
 
-export const useChats = (): ChatState => {
+import { PayloadAction } from "@reduxjs/toolkit";
+
+export const useChats = (): [
+  ChatState,
+  (id: string) => PayloadAction<string, "chats/setActiveChat">
+] => {
+  const dispatch = useAppDispatch();
   const chats = useSelector((state: RootState) => state.chats);
-  return chats;
+
+  const dispatchSetActiveChat = (
+    id: string
+  ): PayloadAction<string, "chats/setActiveChat"> => {
+    return dispatch(setActiveChat(id));
+  };
+
+  return [chats, dispatchSetActiveChat];
 };
 
-export const useActiveChat = (): Chat | undefined => {
-  const { activeChatId, chats } = useChats();
+export const useActiveChat = (): Chat => {
+  const [chatState] = useChats();
+  const { chats, activeChatId } = chatState;
   return chats.find((chat) => chat.id === activeChatId);
 };
