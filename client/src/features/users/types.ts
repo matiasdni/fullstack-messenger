9b01fe3efb6b07dbe579600e1d552e4e847f1700
Matias <matiasdn@metropolia.fi>
2023-05-28 23:44:19 +0300

    changing types to match backend

--- client/src/features/users/types.ts
+++ client/src/features/users/types.ts
@@ -1,24 +1,18 @@
-export type Status = "pending" | "accepted" | "rejected";
-
+import { Status } from "../../../../shared/types";
 export interface Invite {
   id: string;
   sender: User;
-  receiver?: User | string;
-  chat?: {
+  chat: {
     id: string;
-    name?: string;
+    name: string;
   };
-  message: string;
-  status: Status;
-  createdAt?: Date;
+  status?: Status;
+  createdAt: Date;
 }
 
 export interface User {
-  friends?: User[];
-  invites?: Invite[];
   id: string;
   username: string;
-  role: string | null;
 }
 
 export type UsersInitialState = User[];
