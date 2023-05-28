c7cdcf519190721fb2844a01f9afc77367e0284b
Matias <matiasdn@metropolia.fi>
2023-05-29 02:16:30 +0300

    refactor invite interface to features/invites

--- client/src/features/users/types.ts
+++ client/src/features/users/types.ts
@@ -1,15 +1,3 @@
-import { Status } from "../../../../shared/types";
-export interface Invite {
-  id: string;
-  sender: User;
-  chat: {
-    id: string;
-    name: string;
-  };
-  status?: Status;
-  createdAt: Date;
-}
-
 export interface User {
   id: string;
   username: string;
