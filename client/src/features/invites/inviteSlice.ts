14b84c1497dc790ba1b510d61c732599420662c3
Matias <matiasdn@metropolia.fi>
2023-05-29 03:16:12 +0300

    asyncthunk for getting all invites

--- client/src/features/invites/inviteSlice.ts
+++ client/src/features/invites/inviteSlice.ts
@@ -1,5 +1,7 @@
+import { fetchUserRequests } from "src/services/user";
 import { Invite } from "./types";
 import { createSlice, PayloadAction, createAsyncThunk } from "@reduxjs/toolkit";
+import { RootState } from "src/types";
 
 const initialState: Invite[] = [];
 
@@ -16,6 +18,15 @@ export const rejectInvite = createAsyncThunk(
   async (inviteId: string) => {
     // TODO: implement
     return inviteId;
+  }
+);
+
+export const getInvites = createAsyncThunk(
+  "invites/getInvites",
+  async (userId: string, { getState }) => {
+    const token = (getState() as RootState).auth.token;
+    const data = await fetchUserRequests(userId, token);
+    return data;
   }
 );
 
@@ -85,9 +96,27 @@ const invitesSlice = createSlice({
         action
       );
     });
+
+    builder.addCase(getInvites.fulfilled, (state, action) => {
+      const data = action.payload;
+      const invites: Invite[] = data.invites.map((invite) => ({
+        id: invite.id,
+        createdAt: invite.createdAt,
+        sender: data.senders[invite.senderId],
+        chat: data.chats[invite.chatId],
+      }));
+
+      return invites;
+    });
+
+    builder.addCase(getInvites.rejected, (state, action) => {
+      // implement when we have error handling in place and we want to show the user a message
+      console.log("getInvites.rejected", "prev state", state, "action", action);
+    });
   },
 });
 
 export const { addInvite, removeInvite, updateInviteStatus } =
   invitesSlice.actions;
+export const selectInvites = (state: RootState) => state.invites;
 export default invitesSlice.reducer;
