09b63e303e143c4f3fb39ed03357d257691eea63
Matias <matiasdn@metropolia.fi>
2023-05-29 06:35:17 +0300

    implement accepting and rejecting invite

--- server/src/controllers/inviteController.ts
+++ server/src/controllers/inviteController.ts
@@ -1,7 +1,9 @@
 import { Response } from "express";
 import authenticate, { AuthRequest } from "../middlewares/auth";
+import { rejectInvite, updateInvite } from "../services/inviteService";
+import { Invite } from "../../../shared/types";
+import { Chat } from "../models/initModels";
 
-// /api/invites
 const router = require("express").Router();
 
 router.post("/", authenticate, async (req: AuthRequest, res: Response) => {
@@ -14,7 +16,37 @@ router.get("/", authenticate, async (req: AuthRequest, res: Response) => {
 });
 
 router.put("/", authenticate, async (req: AuthRequest, res: Response) => {
-  res.status(200).json({ message: "ok" });
+  const user = req.user;
+  const invite: Invite = req.body;
+  const result = await updateInvite(invite, user);
+
+  const chat = await Chat.findOne({
+    where: { id: result.chatId },
+    include: [
+      {
+        association: "users",
+        attributes: ["id", "username"],
+      },
+    ],
+  });
+  chat!.addUser(user);
+
+  const returnResult = {
+    ...result,
+    chat: {
+      ...chat!.toJSON(),
+      messages: [],
+    },
+  };
+  res.status(200).json(returnResult);
+});
+
+router.delete("/", authenticate, async (req: AuthRequest, res: Response) => {
+  const user = req.user;
+  const invite: Invite = req.body;
+  const result = await rejectInvite({ inviteId: invite.id, userId: user.id });
+  res.status(204).json(result);
+  console.log("result", result, "success");
 });
 
 export default router;
