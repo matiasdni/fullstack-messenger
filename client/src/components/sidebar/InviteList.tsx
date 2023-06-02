5e640b1b5da24adcdfbe8b98c9130ce88810c107
Matias <matiasdn@metropolia.fi>
2023-06-02 17:13:44 +0300

    simplify and refactor to display both: - chat invites - friend requests

--- client/src/components/sidebar/InviteList.tsx
+++ client/src/components/sidebar/InviteList.tsx
@@ -1,52 +1,20 @@
-import { FC, useEffect } from "react";
-import { useAuth } from "src/hooks/useAuth";
+import { FC } from "react";
+import { useUser } from "src/hooks/useAuth";
 import { IoMdClose, IoMdCheckmark } from "react-icons/io";
-import { useAppDispatch, useAppSelector } from "../../store";
+import { useAppDispatch } from "../../store";
 import {
   updateInviteStatus,
-  getInvites,
-  selectInvites,
   rejectInvite,
 } from "src/features/invites/inviteSlice";
 import { Invite } from "src/features/invites/types";
 import { addChat } from "src/features/chats/chatsSlice";
 import { InviteAttributes } from "../../../../shared/types";
-
-const timeSince = (date: Date) => {
-  const seconds = Math.floor((new Date().getTime() - date.getTime()) / 1000);
-  let interval = seconds / 31536000;
-  if (interval > 1) {
-    return Math.floor(interval) + " years";
-  }
-  interval = seconds / 2592000;
-  if (interval > 1) {
-    return Math.floor(interval) + " months";
-  }
-  interval = seconds / 86400;
-  if (interval > 1) {
-    return Math.floor(interval) + " days";
-  }
-  interval = seconds / 3600;
-  if (interval > 1) {
-    return Math.floor(interval) + " hours";
-  }
-  interval = seconds / 60;
-  if (interval > 1) {
-    return Math.floor(interval) + " minutes";
-  }
-  return Math.floor(seconds) + " seconds";
-};
+import { User, friendRequest } from "src/features/users/types";
+import timeSince from "src/utils/timeSince";
 
 const InviteList: FC = () => {
   const dispatch = useAppDispatch();
-  const invites = useAppSelector(selectInvites);
-  const { user } = useAuth();
-  const userId = user.id;
-
-  useEffect(() => {
-    console.log("fetching invites");
-    dispatch(getInvites(userId));
-  }, [dispatch, userId]);
+  const currentUser: User = useUser();
 
   const handleAccept = async (invite: Invite): Promise<void> => {
     const action = await dispatch(
@@ -65,20 +33,48 @@ const InviteList: FC = () => {
     dispatch(rejectInvite(invite));
   };
 
-  const filteredInvites = invites.filter(
-    (invite) => invite.status === "pending"
+  const getPendingInvites = () => {
+    return currentUser.chatInvites.invites
+      .map((invite) => ({
+        id: invite.id,
+        createdAt: invite.createdAt,
+        sender: currentUser.chatInvites.senders[invite.senderId],
+        chat: currentUser.chatInvites.chats[invite.chatId],
+        status: invite.status,
+      }))
+      .filter((invite) => invite.status === "pending");
+  };
+
+  const pendingInvites = getPendingInvites();
+
+  const friendRequestsAndChatInvites = [
+    ...currentUser.friendRequests,
+    ...pendingInvites,
+  ];
+
+  if (!friendRequestsAndChatInvites.length) {
+    return (
+      <div className="flex w-full flex-col space-y-4 divide-y p-2">
+        <p className="text-center">No new invites</p>
+      </div>
+    );
+  }
+
+  const sortedInvites = friendRequestsAndChatInvites.sort(
+    (a: friendRequest | Invite, b: friendRequest | Invite) =>
+      new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
   );
 
   return (
     <div className="flex w-full flex-col space-y-4 divide-y p-2">
-      {filteredInvites.map((invite) => (
+      {sortedInvites.map((pendingInvite) => (
         <div
-          key={invite.id}
+          key={pendingInvite.id}
           className="flex w-full flex-row items-center justify-center space-x-2"
         >
           <div className="shrink-0">
             <img
-              src={`https://avatars.dicebear.com/api/identicon/${invite.sender.username}.svg`}
+              // src={`https://avatars.dicebear.com/api/identicon/${invite.sender.username}.svg`}
               alt="avatar"
               className="h-8 w-8 rounded-full object-cover"
             />
@@ -86,28 +82,28 @@ const InviteList: FC = () => {
           <div className="w-full">
             <div className="flex w-full items-center justify-between">
               <p className="font-semibold text-black dark:text-gray-300">
-                {invite.sender.username}
+                {/* {invite.sender.username} */}
               </p>
             </div>
             <div className="flex flex-col space-y-2">
               <p className="text-sm text-gray-500">
-                invited you to join <span>{invite.chat.name}</span>
+                {/* invited you to join <span>{invite.chat.name}</span> */}
               </p>
             </div>
             <p className="text-xs text-gray-500">
-              {timeSince(new Date(invite.createdAt))} ago
+              {timeSince(new Date(pendingInvite.createdAt))} ago
             </p>
           </div>
           <div className="space-y-1">
             <IoMdCheckmark
               className="fill-emerald-500 hover:fill-green-800"
               size={24}
-              onClick={() => handleAccept(invite)}
+              // onClick={() => handleAccept(invite)}
             />
             <IoMdClose
               className=" fill-rose-500 hover:fill-rose-900"
               size={24}
-              onClick={() => handleReject(invite)}
+              // onClick={() => handleReject(invite)}
             />
           </div>
         </div>
