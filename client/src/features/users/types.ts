54447cd7642992e4dafc1f892c3cd700abd4a965
Matias <matiasdn@metropolia.fi>
2023-06-02 14:32:34 +0300

    add fields to user - friends - chat invites - friend requests

--- client/src/features/users/types.ts
+++ client/src/features/users/types.ts
@@ -1,6 +1,11 @@
+import { Invite } from "../../../../shared/types";
+
 export interface User {
   id: string;
   username: string;
+  friends?: User[];
+  invites?: Invite[];
+  friendRequests?: User[];
 }
 
 export type UsersInitialState = User[];
