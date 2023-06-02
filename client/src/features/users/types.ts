f8a5fd474eec4aa1c9a68bef2e8b2617e721996c
Matias <matiasdn@metropolia.fi>
2023-06-02 16:59:03 +0300

    add type for friend request

--- client/src/features/users/types.ts
+++ client/src/features/users/types.ts
@@ -1,11 +1,19 @@
-import { Invite } from "../../../../shared/types";
+import { GetPendingInvitesOutput } from "../../../../shared/types";
+
+export interface friendRequest {
+  id: string;
+  userId: string;
+  username: string;
+  status: "pending" | "accepted" | "rejected";
+  createdAt: Date;
+}
 
 export interface User {
   id: string;
   username: string;
   friends?: User[];
-  chatInvites?: Invite[];
-  friendRequests?: User[];
+  chatInvites?: GetPendingInvitesOutput;
+  friendRequests?: friendRequest[];
 }
 
 export type UsersInitialState = User[];
