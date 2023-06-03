dd7b907c36301b99b6131749af8c5d177454974d
matiasdn <matiasdn@metropolia.fi>
2023-06-03 20:35:40 +0300

    use pino for logging

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,16 @@
 import { server } from "./server";
 import "./models/initModels";
 import { initModels } from "./models/initModels";
+import logger from "./utils/logger";
 
 const PORT = process.env.PORT || 3001;
 
 initModels()
   .then(() => {
     server.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
+      logger.info(`Server is running on port ${PORT}`);
     });
   })
   .catch((err) => {
-    console.log(err);
+    logger.error(err);
   });
