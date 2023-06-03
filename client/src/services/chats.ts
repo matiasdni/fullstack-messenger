3bde455695e9398de5e24448e9c87f9b09d7ba41
Matias <matiasdn@metropolia.fi>
2023-06-03 08:28:05 +0300

    implement sendMessage to post messages to backend

--- client/src/services/chats.ts
+++ client/src/services/chats.ts
@@ -11,7 +11,7 @@ export interface chatData {
   description?: string;
 }
 
-export const newChat = async (chatData: chatData, token): Promise<Chat> => {
+const newChat = async (chatData: chatData, token): Promise<Chat> => {
   console.log("chatData", chatData);
   const response = await axios.post(BASE_URL, chatData, {
     withCredentials: true,
@@ -24,7 +24,7 @@ export const newChat = async (chatData: chatData, token): Promise<Chat> => {
   else throw new Error("chat creation failed");
 };
 
-export const fetchChatById = async (chatId, token): Promise<Chat> => {
+const fetchChatById = async (chatId, token): Promise<Chat> => {
   const response = await axios.get(`${BASE_URL}/${chatId}`, {
     withCredentials: true,
     headers: {
@@ -35,3 +35,22 @@ export const fetchChatById = async (chatId, token): Promise<Chat> => {
   if (response.status === 200) return response.data;
   else throw new Error("chat creation failed");
 };
+
+const sendMessage = async (chatId: string, message: string, token: string) => {
+  const response = await axios.post(
+    `${BASE_URL}/${chatId}/message`,
+    {
+      message,
+    },
+    {
+      withCredentials: true,
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+  if (response.status === 200) return response.data;
+  else throw new Error("message sending failed");
+};
+
+export { newChat, fetchChatById, sendMessage };
