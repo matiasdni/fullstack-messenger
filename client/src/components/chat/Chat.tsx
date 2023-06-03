a1d239679ee62c852c12198a7880e4f67b7867e7
Matias <matiasdn@metropolia.fi>
2023-06-03 08:26:32 +0300

    refactoring sendMessage to form component

--- client/src/components/chat/Chat.tsx
+++ client/src/components/chat/Chat.tsx
@@ -1,27 +1,14 @@
-import { socket } from "../../socket";
 import { useAppSelector } from "../../store";
 import { selectActiveChat } from "../../features/chats/chatsSlice";
 import { Chat as ChatType } from "../../features/chats/types";
 import { ChatHeader } from "./ChatHeader";
 import { MessageList } from "./MessageList";
-import { ChatInputForm } from "./ChatInputForm";
+import ChatInputForm from "./ChatInputForm";
+import { useToken } from "src/hooks/useAuth";
 
 export const Chat = () => {
+  const token = useToken();
   const activeChat: ChatType = useAppSelector(selectActiveChat);
-
-  const sendMessage = (input: string) => {
-    console.log("sending message");
-    if (input.trim().length > 0) {
-      try {
-        socket.timeout(2000).emit("message", {
-          content: input,
-          room: activeChat?.id,
-        });
-      } catch (e) {
-        console.log(e);
-      }
-    }
-  };
 
   return activeChat ? (
     <div className="flex h-full w-full flex-col ">
@@ -31,7 +18,7 @@ export const Chat = () => {
       </div>
 
       {/*input*/}
-      <ChatInputForm onSubmit={sendMessage} />
+      <ChatInputForm activeChat={activeChat} token={token} />
     </div>
   ) : (
     <div className="flex h-full w-full flex-col divide-y divide-neutral-200 dark:divide-neutral-700  dark:bg-gray-700/5 ">
