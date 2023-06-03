003d66065ccf34eeae2dd8693af51c7837346f96
Matias <matiasdn@metropolia.fi>
2023-06-03 08:29:10 +0300

    add post path to route for messages

--- server/src/routes/chats.ts
+++ server/src/routes/chats.ts
@@ -1,4 +1,8 @@
-import { chatById, createChat } from "../controllers/chatController";
+import {
+  chatById,
+  createChat,
+  sendMessage,
+} from "../controllers/chatController";
 import authenticate from "../middlewares/auth";
 
 const router = require("express").Router();
@@ -8,5 +12,7 @@ router.use(authenticate);
 router.post("/", createChat);
 
 router.get("/:id", chatById);
+
+router.post("/:id/message", sendMessage);
 
 export default router;
