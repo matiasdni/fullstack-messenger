f5f0f7ac009291d4197e2dd333edd07b6adefcb7
Matias <matiasdn@metropolia.fi>
2023-06-05 05:53:47 +0300

    log error

--- server/src/middlewares/errorHandler.ts
+++ server/src/middlewares/errorHandler.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
-import ApiError from "../utils/apiError";
+import { ApiError } from "../utils/ApiError";
+import logger from "../utils/logger";
 
 const errorHandler = (
   err: any,
@@ -8,6 +9,7 @@ const errorHandler = (
   next: NextFunction
 ) => {
   if (err instanceof ApiError) {
+    logger.error(err);
     res.status(err.statusCode).json({ error: err.message });
   } else {
     res.status(500).json({ error: "Unexpected error" });
