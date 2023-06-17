360e53a0e4f9624e68eae9e3699a73065e587865
Matias <matiasdn@metropolia.fi>
2023-06-17 21:11:02 +0300

    implemented user profile to display in drawer

--- client/src/components/profile/UserProfile.tsx
+++ client/src/components/profile/UserProfile.tsx
@@ -1,26 +1,46 @@
-import { useUser } from "hooks/useAuth";
 import { UserDetails } from "components/profile/UserDetails";
 import { UserProfileHeader } from "components/profile/UserProfileHeader";
+import { useUser } from "hooks/useAuth";
+import { useEffect, useState } from "react";
 
 export const UserProfile = () => {
+  const [editMode, setEditMode] = useState<boolean>(false);
   const user = useUser();
+  const [image, setImage] = useState<string>(user?.image);
+  const [formData, setFormData] = useState<FormData>(new FormData());
+  const [bio, setBio] = useState<string>(user?.bio);
+
   const current = {
     ...user,
     bio: "I am a cool person",
     github: "matiasdni",
   };
+
+  useEffect(() => {
+    setBio(user?.bio);
+    setImage(user.image);
+    console.log(user);
+  }, [editMode, user]);
+
   return (
     <>
-      <dialog id="user_profile" className="modal modal-bottom sm:modal-middle">
-        <form method="dialog" className="modal-box">
-          <UserProfileHeader current={current} user={user} />
-          <UserDetails current={current} />
-          <div className="modal-action"></div>
-        </form>
-        <form method="dialog" className="modal-backdrop">
-          <button>close</button>
-        </form>
-      </dialog>
+      <form className="flex flex-col p-4 ">
+        <header>
+          <UserProfileHeader
+            current={current}
+            user={user}
+            editMode={editMode}
+            setEditMode={setEditMode}
+            image={image}
+            setImage={setImage}
+            formData={formData}
+            setFormData={setFormData}
+          />
+        </header>
+        <section>
+          <UserDetails user={user} />
+        </section>
+      </form>
     </>
   );
 };
--- client/src/components/profile/UserProfileHeader.tsx
+++ client/src/components/profile/UserProfileHeader.tsx
@@ -1,7 +1,11 @@
 import { friendRequest, User } from "features/users/types";
+import React, { useCallback } from "react";
+import { FaGithub } from "react-icons/fa";
 import { GetPendingInvitesOutput } from "../../../../shared/types";
 
 export function UserProfileHeader(props: {
+  editMode: boolean;
+  setEditMode: React.Dispatch<React.SetStateAction<boolean>>;
   current: {
     friendRequests?: friendRequest[];
     image?: string;
@@ -14,24 +18,108 @@ export function UserProfileHeader(props: {
     username: string;
   };
   user: User;
+  image: string;
+  setImage: React.Dispatch<React.SetStateAction<string>>;
+  formData: FormData;
+  setFormData: React.Dispatch<React.SetStateAction<FormData>>;
 }) {
+  const { editMode, setEditMode, image, setImage, setFormData } = props;
+
+  const handleImageChange = useCallback(async (e) => {
+    if (e.target.files) {
+      const image: File = e.target.files[0];
+      setFormData((prev) => {
+        prev.set("image", image, image.name);
+        return prev;
+      });
+      const imageUrl = URL.createObjectURL(image);
+      setImage(imageUrl);
+    }
+  }, []);
+
   return (
-    <header>
-      <button className="btn-sm btn-circle btn-ghost absolute right-2 top-2">
+    <>
+      <button
+        className="absolute btn-sm btn-circle btn-ghost right-2 top-2"
+        onClick={(e) => {
+          e.preventDefault();
+          document.getElementById("drawer-overlay")?.click();
+        }}
+      >
         ✕
       </button>
-      <h3 className="font-bold text-lg">{props.current?.username}'s Profile</h3>
-      <div className="flex flex-col items-center justify-center">
-        <img
-          className="w-32 h-32 rounded-full"
-          src={`https://avatars.dicebear.com/api/identicon/${props.user?.username}.svg`}
-          alt=""
-        />
-        <p className="text-lg">{props.current?.username}</p>
-        <p className="text-sm text-neutral-content">
-          user since: {new Date().toLocaleString()}
-        </p>
+      <div className="flex items-center justify-between my-6">
+        <h3 className="text-lg font-bold">Profile</h3>
+        {/* Edit profile button */}
+        <button
+          className="btn btn-xs btn-primary"
+          onClick={(e) => {
+            e.preventDefault();
+            return setEditMode(!editMode);
+          }}
+        >
+          {editMode ? "Cancel" : "Edit"}
+        </button>
       </div>
-    </header>
+      <div className="flex flex-col items-center justify-center space-y-1">
+        {/* Avatar */}
+        <label
+          htmlFor="avatar"
+          className="relative w-24 h-24 overflow-hidden transition-all border-0 rounded-full shadow-md cursor-pointer select-none dark:bg-gray-600 btn-ghost avatar aspect-1 btn-circle mask hover:shadow-lg"
+        >
+          {/* Avatar */}
+          <img
+            src={
+              image ||
+              "https://avatars.dicebear.com/api/avataaars/John%20Doe.svg?mood[]=happy"
+            }
+            alt="avatar"
+            className="h-full"
+          />
+          {/* Change avatar button */}
+          <div className="absolute inset-0 flex items-center justify-center text-sm text-center text-white align-middle transition-opacity bg-black bg-opacity-50 opacity-0 hover:opacity-100">
+            <span className="absolute top-0 bottom-0 w-full h-full m-auto leading-tight text-center -translate-x-1/2 translate-y-1/3 start-1/2">
+              <p className="leading-[.6rem] font-bold text-[.7rem] uppercase subpixel-antialiased pt-2 select-none tracking-tight">
+                Change
+                <br />
+                Avatar
+              </p>
+            </span>
+
+            {/* File input to change avatar */}
+            <input
+              id="avatar"
+              type="file"
+              hidden
+              className="absolute inset-0 w-full h-full opacity-0 cursor-pointer"
+              onChange={handleImageChange}
+            />
+          </div>
+        </label>
+
+        {/* Username */}
+        <h3 className="text-xl font-bold">{props.current.username}</h3>
+
+        {/* Bio */}
+        <p className="text-sm text-center">{props.current.bio}</p>
+      </div>
+
+      {/* GitHub link */}
+      <div className="flex items-center justify-between">
+        <div className="flex items-center justify-between mt-4">
+          <a
+            href={`github.com/${props.current.github}`}
+            onClick={(e) => {
+              e.preventDefault();
+              window.open(`https://www.github.com/${props.current.github}`);
+            }}
+            className="flex items-center justify-between mt-4 space-x-1"
+          >
+            <FaGithub />
+            <span className="text-sm font-bold">Github</span>
+          </a>
+        </div>
+      </div>
+    </>
   );
 }
