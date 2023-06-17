36248a0c8eef1a0e222606a0f64d44e55196afd7
Matias <matiasdn@metropolia.fi>
2023-06-17 21:11:58 +0300

    wrapped headers header inside header tag

--- client/src/components/sidebar/SidebarHeader.tsx
+++ client/src/components/sidebar/SidebarHeader.tsx
@@ -1,27 +1,22 @@
 import { useDrawer } from "@/contexts/DrawerContext";
 import Tooltip from "components/common/Tooltip";
-import { useUser } from "hooks/useAuth";
 import { FC, useState } from "react";
 import { IoIosNotifications } from "react-icons/io";
 import { IconContext } from "react-icons/lib";
 import { MdGroupAdd, MdOutlineMessage } from "react-icons/md";
-import { GroupForm } from "../GroupForm";
-import { UserSearch } from "../UserSearch";
-import { Modal } from "../common/Modal";
 import { Tab } from "./SidebarTab";
+import { Modal } from "components/common/Modal";
+import { UserSearch } from "components/UserSearch";
+import { GroupForm } from "components/GroupForm";
 
 interface SidebarHeaderProps {
   activeTab: Tab;
   onChangeTab: (tab: Tab) => void;
 }
 
-const SidebarHeader: FC<SidebarHeaderProps> = ({
-  activeTab,
-  onChangeTab,
-}: SidebarHeaderProps) => {
+const SidebarHeader: FC<SidebarHeaderProps> = ({ activeTab, onChangeTab }) => {
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   const [isGroupModalOpen, setIsGroupModalOpen] = useState<boolean>(false);
-  const user = useUser();
   const { setDrawerContent } = useDrawer();
 
   const handleGroupModalClose = (): void => {
@@ -31,57 +26,59 @@ const SidebarHeader: FC<SidebarHeaderProps> = ({
   const setDrawerContext = (): void => setDrawerContent("notifications");
 
   return (
-    <div className="p-2 space-y-4 bg-neutral-100 dark:bg-gray-800">
-      {/* Header */}
-      <div className="flex items-center justify-between mx-1 my-2">
-        {/* Tab title */}
-        <h2 className="w-full text-xl font-semibold text-gray-900 capitalize dark:text-gray-50">
-          {activeTab}
-        </h2>
+    <>
+      <header className="p-2 space-y-4 bg-neutral-100 dark:bg-gray-800">
+        {/* Header */}
+        <div className="flex items-center justify-between mx-1 my-2">
+          {/* Tab title */}
+          <h2 className="w-full text-xl font-semibold text-gray-900 capitalize dark:text-gray-50">
+            {activeTab}
+          </h2>
 
-        {/* action buttons */}
-        <div className="space-x-2 join join-horizontal">
-          <IconContext.Provider
-            value={{
-              className:
-                "text-gray-500 join-item dark:text-gray-400 h-6 w-6 hover:text-gray-900 dark:hover:text-gray-50 cursor-pointer align-middle transition-colors duration-200 ease-in-out",
-            }}
-          >
-            {/* Start a chat icon */}
-            <Tooltip label="Start a Chat">
-              <MdOutlineMessage
-                onClick={(): void => {
-                  setIsModalOpen(true);
-                }}
-              />
-            </Tooltip>
+          {/* action buttons */}
+          <div className="space-x-2 join join-horizontal">
+            <IconContext.Provider
+              value={{
+                className:
+                  "text-gray-500 join-item dark:text-gray-400 h-6 w-6 hover:text-gray-900 dark:hover:text-gray-50 cursor-pointer align-middle transition-colors duration-200 ease-in-out",
+              }}
+            >
+              {/* Start a chat icon */}
+              <Tooltip label="Start a Chat">
+                <MdOutlineMessage
+                  onClick={(): void => {
+                    setIsModalOpen(true);
+                  }}
+                />
+              </Tooltip>
 
-            {/* Create group icon */}
-            <Tooltip label="Create Group">
-              <MdGroupAdd
-                onClick={(): void => {
-                  setIsGroupModalOpen(true);
-                }}
-              />
-            </Tooltip>
+              {/* Create group icon */}
+              <Tooltip label="Create Group">
+                <MdGroupAdd
+                  onClick={(): void => {
+                    setIsGroupModalOpen(true);
+                  }}
+                />
+              </Tooltip>
 
-            {/* Notifications icon */}
-            <Tooltip label="Notifications">
-              <label htmlFor="my-drawer" className="drawer-button">
-                <IoIosNotifications onClick={setDrawerContext} />
-              </label>
-            </Tooltip>
-          </IconContext.Provider>
+              {/* Notifications icon */}
+              <Tooltip label="Notifications">
+                <label htmlFor="my-drawer" className="drawer-button">
+                  <IoIosNotifications onClick={setDrawerContext} />
+                </label>
+              </Tooltip>
+            </IconContext.Provider>
+          </div>
         </div>
-      </div>
 
-      {/* Search input */}
-      <div className="pb-1">
-        <input
-          className="min-w-full p-1 space-y-0 text-left border border-gray-400 rounded shadow-sm form-input bg-neutral-100 dark:border-gray-600 dark:bg-gray-700"
-          placeholder="Search for users"
-        />
-      </div>
+        {/* Search input */}
+        <div className="pb-1">
+          <input
+            className="min-w-full p-1 space-y-0 text-left border border-gray-400 rounded shadow-sm form-input bg-neutral-100 dark:border-gray-600 dark:bg-gray-700"
+            placeholder="Search for users"
+          />
+        </div>
+      </header>
 
       {/*
         TODO: Refactor this to parent container where the main content is
@@ -104,7 +101,7 @@ const SidebarHeader: FC<SidebarHeaderProps> = ({
           />
         </Modal>
       )}
-    </div>
+    </>
   );
 };
 
