fed2fff874cb4c363c589183c6bb1022b323a452
Matias <matiasdn@metropolia.fi>
2023-06-18 06:06:06 +0300

    remove redundant dispatch hook

--- client/src/store.ts
+++ client/src/store.ts
@@ -1,19 +1,10 @@
-import { configureStore, ThunkDispatch } from "@reduxjs/toolkit";
-import { ActionsFromAsyncThunk } from "@reduxjs/toolkit/src/matchers";
+import { configureStore } from "@reduxjs/toolkit";
 import notificationReducer from "features/notification/notificationSlice";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import { RootState } from "types";
-import authReducer, { login } from "./features/auth/authSlice";
-import chatsReducer, {
-  createChat,
-  getChatById,
-  getChats,
-} from "./features/chats/chatsSlice";
-import inviteReducer, {
-  getInvites,
-  rejectInvite,
-  updateInviteStatus,
-} from "./features/invites/inviteSlice";
+import authReducer from "./features/auth/authSlice";
+import chatsReducer from "./features/chats/chatsSlice";
+import inviteReducer from "./features/invites/inviteSlice";
 import usersReducer from "./features/users/usersSlice";
 
 const store = configureStore({
@@ -28,19 +19,6 @@ const store = configureStore({
 
 export type AppDispatch = typeof store.dispatch;
 export const useAppDispatch = () => useDispatch<AppDispatch>();
-export const useThunkDispatch = (): ThunkDispatch<
-  RootState,
-  void,
-  ActionsFromAsyncThunk<
-    | typeof createChat
-    | typeof getChats
-    | typeof updateInviteStatus
-    | typeof rejectInvite
-    | typeof getInvites
-    | typeof getChatById
-    | typeof login
-  >
-> => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
 export default store;
