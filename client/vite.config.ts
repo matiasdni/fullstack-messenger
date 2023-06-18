f349c304d076f963efe2f37c0a79b1809ab5758f
Matias <matiasdn@metropolia.fi>
2023-06-18 20:02:53 +0300

    update react-router, react-router-dom, tailwind-merge and vitest to latest version

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,7 +6,6 @@ import { checker } from "vite-plugin-checker";
 import svgrPlugin from "vite-plugin-svgr";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   envPrefix: "REACT_APP_",
   build: {
