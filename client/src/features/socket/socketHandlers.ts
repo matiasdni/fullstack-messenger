776a1100c085b6f142c4f333f05e94143a7118e7
matiasdn <matiasdn@metropolia.fi>
2023-06-18 13:00:00 +0300

    extracting call back functions to own file

---
+++ client/src/features/socket/socketHandlers.ts
@@ -0,0 +1,86 @@
+import { socket } from "socket";
+import { AppDispatch } from "store";
+import React from "react";
+import { Chat, Message } from "features/chats/types";
+import {
+  addMessage,
+  getChatById,
+  removeChat,
+  updateChat,
+} from "features/chats/chatsSlice";
+import {
+  addChatInvite,
+  addFriend,
+  addFriendRequest,
+  removeFriendRequest,
+  updateUser,
+} from "features/auth/authSlice";
+import { friendRequest, User } from "features/users/types";
+
+export const onConnect =
+  (chatsRef: React.MutableRefObject<Chat[]>) => (): void => {
+    console.log("socket connected");
+    chatsRef.current.forEach((chat: Chat) => socket.emit("join-room", chat.id));
+  };
+
+export function onDisconnect() {
+  return () => console.log("socket disconnected");
+}
+
+export function onMessage(
+  chatsRef: React.MutableRefObject<Chat[]>,
+  dispatch: AppDispatch
+) {
+  return async (data: Message): Promise<void> => {
+    console.log("message received");
+    const chat = chatsRef.current.find((chat) => chat.id === data.chatId);
+    if (!chat) {
+      await dispatch(getChatById(data.chatId));
+    } else {
+      dispatch(addMessage(data));
+    }
+  };
+}
+
+export function onFriendRequest(dispatch: AppDispatch) {
+  return (data: any) => {
+    console.log("friend request received");
+    dispatch(addFriendRequest(data));
+  };
+}
+
+export function onFriendRequestAccepted(dispatch: AppDispatch) {
+  return (data: { friendRequest: friendRequest; newFriend: User }) => {
+    console.log("friend request accepted");
+    dispatch(addFriend(data.newFriend));
+    dispatch(removeFriendRequest(data.friendRequest));
+  };
+}
+
+export function onChatInvite(dispatch: AppDispatch) {
+  return (data: any) => dispatch(addChatInvite(data));
+}
+
+export function onUserUpdate(dispatch: AppDispatch) {
+  return (data) => dispatch(updateUser(data));
+}
+
+export function onChatUpdate(dispatch: AppDispatch) {
+  return (data) => dispatch(updateChat(data));
+}
+
+export function onLeaveRoom(dispatch: AppDispatch) {
+  return (data) => dispatch(removeChat(data));
+}
+
+export default {
+  onConnect,
+  onDisconnect,
+  onMessage,
+  onFriendRequest,
+  onFriendRequestAccepted,
+  onChatInvite,
+  onUserUpdate,
+  onChatUpdate,
+  onLeaveRoom,
+} as const;
---
+++ client/src/hooks/useSocket.ts
@@ -0,0 +1,43 @@
+import { useEffect, useRef } from "react";
+import { socket } from "socket";
+import { useAppDispatch, useAppSelector } from "store";
+import * as socketHandlers from "features/socket/socketHandlers";
+
+const useSocket = () => {
+  const dispatch = useAppDispatch();
+  const chats = useAppSelector((state) => state.chats.chats);
+  const chatsRef = useRef(chats);
+
+  useEffect(() => {
+    chatsRef.current = chats;
+  }, [chats]);
+
+  useEffect(() => {
+    const eventHandlers = {
+      connect: socketHandlers.onConnect(chatsRef),
+      disconnect: socketHandlers.onDisconnect,
+      message: socketHandlers.onMessage(chatsRef, dispatch),
+      "friend-request": socketHandlers.onFriendRequest(dispatch),
+      "friend-request-accepted":
+        socketHandlers.onFriendRequestAccepted(dispatch),
+      userUpdate: socketHandlers.onUserUpdate(dispatch),
+      chatUpdate: socketHandlers.onChatUpdate(dispatch),
+      "chat-invite": socketHandlers.onChatInvite(dispatch),
+      "leave-room": socketHandlers.onLeaveRoom,
+    };
+
+    Object.entries(eventHandlers).forEach(([event, handler]) => {
+      socket.on(event, handler);
+    });
+
+    return () => {
+      Object.entries(eventHandlers).forEach(([event]) => {
+        socket.off(event);
+      });
+    };
+  }, [dispatch]);
+
+  return socket;
+};
+
+export default useSocket;
