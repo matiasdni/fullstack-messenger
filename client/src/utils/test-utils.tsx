101786e1f69e6db368bff0237d1994f6168219dd
Matias <matiasdn@metropolia.fi>
2023-06-19 03:38:40 +0300

    setup and custom render function with providers

---
+++ client/src/utils/test-utils.tsx
@@ -0,0 +1,48 @@
+import { DrawerProvider } from "@/contexts/DrawerContext";
+import { configureStore } from "@reduxjs/toolkit";
+import { cleanup, render } from "@testing-library/react";
+import authReducer from "features/auth/authSlice";
+import chatsReducer from "features/chats/chatsSlice";
+import inviteReducer from "features/invites/inviteSlice";
+import notificationReducer from "features/notification/notificationSlice";
+import usersReducer from "features/users/usersSlice";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { afterEach } from "vitest";
+
+afterEach(() => {
+  cleanup();
+});
+
+function customRenderWithProviders(
+  ui: React.ReactElement,
+  {
+    preloadedState = {},
+    store = configureStore({
+      reducer: {
+        auth: authReducer,
+        chats: chatsReducer,
+        users: usersReducer,
+        invites: inviteReducer,
+        notifications: notificationReducer,
+      },
+      preloadedState,
+    }),
+    ...renderOptions
+  } = {}
+) {
+  return render(ui, {
+    ...renderOptions,
+    wrapper: ({ children }) => (
+      <Provider store={store}>
+        <BrowserRouter>
+          <DrawerProvider>{children}</DrawerProvider>
+        </BrowserRouter>
+      </Provider>
+    ),
+  });
+}
+
+export * from "@testing-library/react";
+export { default as userEvent } from "@testing-library/user-event";
+export { customRenderWithProviders as renderWithProviders };
