281bb1296dc4b1bf2535fb99e163b2370e8fe833
Matias <matiasdn@metropolia.fi>
2023-06-19 03:39:36 +0300

    export selector for notificationState

--- client/src/store.ts
+++ client/src/store.ts
@@ -21,4 +21,6 @@ export type RootState = ReturnType<typeof store.getState>;
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
+export const notificationState = (state: RootState) => state.notifications;
+
 export default store;
