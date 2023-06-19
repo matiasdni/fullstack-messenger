fd0f97f4d6c7584e50f0929f31830ed726c79fe8
Matias <matiasdn@metropolia.fi>
2023-06-19 08:40:45 +0300

    add another parameter as optional

--- client/src/utils/timeSince.ts
+++ client/src/utils/timeSince.ts
@@ -1,10 +1,11 @@
 /**
  * Returns a string representing the time elapsed since the given date.
  * @param date The date to calculate the elapsed time from.
+ * @param now The current time (optional, defaults to the current time).
  * @returns A string representing the elapsed time in years, months, days, hours, minutes, or seconds.
  */
-const timeSince = (date: Date) => {
-  const seconds = Math.floor((new Date().getTime() - date.getTime()) / 1000);
+const timeSince = (date: Date, now: Date = new Date()) => {
+  const seconds = Math.floor((now.getTime() - date.getTime()) / 1000);
   let interval = seconds / 31536000;
   if (interval > 1) {
     return Math.floor(interval) + " years";
